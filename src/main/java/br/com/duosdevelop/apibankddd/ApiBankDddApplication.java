a470e2b472a351409c2743361a4b26cd2552ba5f
Gerson Junior <gersonjunior1771@gmail.com>
2020-05-17 10:15:54 -0300

    Criação de projeto

---
+++ src/main/java/br/com/duosdevelop/apibankddd/ApiBankDddApplication.java
@@ -0,0 +1,13 @@
+package br.com.duosdevelop.apibankddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBankDddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBankDddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/duosdevelop/apibankddd/ApiBankDddApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.duosdevelop.apibankddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiBankDddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
